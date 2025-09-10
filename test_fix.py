39487ed23626fb3d77514df2e756ff710bd2935c
Oluwafemi Idiakhoa <oluwafemidiakhoa@gmail.com>
2025-09-10 15:51:06 -0500

    chore(test): fix import order in test_fix (ruff I001)

--- test_fix.py
+++ test_fix.py
@@ -1,11 +1,13 @@
+# test_fix.py
 import sys
 
 from src.sundew.core import SundewAlgorithm
 from tests.conftest import get_preset
 
 # Remove any cached versions
-for key in [k for k in list(sys.modules) if "sundew" in k]:
-    del sys.modules[key]
+modules_to_remove = [k for k in list(sys.modules) if "sundew" in k]
+for m in modules_to_remove:
+    del sys.modules[m]
 
 cfg = get_preset("tuned_v2", overrides=dict(gate_temperature=0.15, energy_pressure=0.04))
 algo = SundewAlgorithm(cfg)
